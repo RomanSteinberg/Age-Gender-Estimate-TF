f2dbd1112fa710eb730ab9051fbfd7545bfbcb31
Alexey <chernysh@visionsystems.tech>
2019-06-18 14:52:06 +0300

    feat: add positive and negative mining

--- mining.py
+++ mining.py
@@ -1,39 +1,49 @@
 import os
-import cv2
-import dlib
 import json
 import numpy as np
 import tensorflow as tf
+from pathlib import Path
 from age_gender.utils.config_parser import get_config
 from age_gender.utils.model_saver import ModelSaver
 from age_gender.nets.inception_resnet_v1 import InceptionResnetV1
-# from age_gender.nets.old_inception import inference
-# from age_gender.nets.new_inception import inference as new_inference, Inception
 from age_gender.utils.converter import Converter
-from age_gender.utils.dataloader import DataLoader
-import argparse
+from tqdm import tqdm
 
-def validate(config, converter, sess, age, gender, images_pl):
-    dataset_path = config['dataset_path']
-    with open(dataset_path) as f:
-        dataset_json = json.load(f)
-    dataset = dataset_json[:5]
-    print(dataset)
 
-    # for batch_idx in range((test_size + 1) // self.batch_size):
-    #     test_images, test_age_labels, test_gender_labels, _ = sess.run(next_test_data)
-    path_to_images = os.path.dirname(dataset_path)
-    faces = np.empty((len(dataset), 256, 256, 3))
-    for ind, record in enumerate(dataset):
-        image_path = os.path.join(path_to_images, record['file_name'])
-        face = converter.convert_image(image_path)
-        faces[ind, :, :, :] = face
+def metric(gt_age, pred_age):
+    return abs(gt_age - pred_age)
 
-    ages, genders = sess.run([age, gender], feed_dict={images_pl: faces})
-    print('ages: ', ages)
-    print('genders: ', genders)
+class Miner:
+    def __init__(self, config, converter, sess):
+        self.config = config
+        self.converter = converter
+        self.sess = sess
+        preds = None
 
-def load_network_new(config):
+    def run(self):
+        dataset_path = self.config['dataset_path']
+        dataset = json.load(Path(dataset_path).open())
+        dataset = dataset[:5]
+        print(dataset)
+
+        path_to_images = os.path.dirname(dataset_path)
+
+        for item in tqdm(dataset):
+            image_path = os.path.join(path_to_images, item['file_name'])
+            face = converter.convert_image(image_path)
+            faces = np.empty((1, 256, 256, 3))
+            faces[0, :, :, :] = face
+            pred_age, pred_gender = sess.run([age, gender], feed_dict={images_pl: faces})
+            err = metric(item['age'], pred_age[0].tolist())
+            item.update({'pred_age': pred_age[0].tolist(), 'pred_gender': pred_gender[0].tolist(), 'err': err})
+        Path(config['results_path']).mkdir(exist_ok = True)
+        json.dump(dataset, Path(config['results_path']).joinpath('all_results.json').open(mode='w'))
+        negative = sorted(dataset, key=lambda row: row['err'], reverse=True)[:self.config['negative_size']]
+        json.dump(negative, Path(config['results_path']).joinpath('negative.json').open(mode='w'))
+        positive = sorted(dataset, key=lambda row: row['err'], reverse=False)[:self.config['positive_size']]
+        json.dump(positive, Path(config['results_path']).joinpath('positive.json').open(mode='w'))
+
+def load_network(config):
     pretrained_model_folder_or_file = config['pretrained_model_folder_or_file']
     sess = tf.Session()
     images_pl = tf.placeholder(tf.float32, shape=[None, 256, 256 ,3], name='input_image')
@@ -50,19 +60,14 @@ def load_network_new(config):
     saver.restore_model(sess, pretrained_model_folder_or_file)
     return sess, age, gender, images_pl
 
-if __name__ == '__main__':
-    # parser = argparse.ArgumentParser()
-    # parser.add_argument('--dataset', type=str, help='path to json for dataset')
-    # parser.add_argument('--images', type=str, help='path to images')
-    # parser.add_argument('--output', type=str, help='path to output json')
-    # args = parser.parse_args()
 
-    config = get_config('config.yaml','validate')
+if __name__ == '__main__':
+    config = get_config('config.yaml','mining')
     converter_config = get_config('config.yaml','prepare')
     converter = Converter(converter_config)
     if not config['cuda']:
         os.environ['CUDA_VISIBLE_DEVICES'] = ''
-    sess, age, gender, images_pl = load_network_new(config)
-    validate(config, converter, sess,  age, gender, images_pl)
+    sess, age, gender, images_pl = load_network(config)
+    Miner(config, converter, sess).run()
 
-def get_results(predictions, type, size):
+
